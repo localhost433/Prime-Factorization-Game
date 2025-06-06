f40a46d14e253311b016983a6475a129db27ee78
Robin <87485406+localhost433@users.noreply.github.com>
2025-06-06 18:23:23 -0400

    Update supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -5,4 +5,4 @@ export const supabase = createClient(
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1YXBoaGV1cnd0enRpeGZ0Z21xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxOTMxMDEsImV4cCI6MjA2NDc2OTEwMX0.hMz3q9TvvdO2CxdNm9H1QaCntBG7eGmvUIwSDxiK35g'
 );
 
-export const LEADERBOARD_TABLE = "Factorization-Game-Leaderboard";
+export const LEADERBOARD_TABLE = "leaderboard";
